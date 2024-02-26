50c9bee87befdc11809f516e50e464f6912f97a0
batun <batun@BATUN>
2024-02-26 19:04:30 +0300

    Add project files.

---
+++ Business/Concrete/AdminManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        private IAdminDal _adminDal;
+
+        public AdminManager(IAdminDal adminDal)
+        {
+            _adminDal=adminDal;
+        }
+
+        public void Add(Admin admin)
+        {
+            _adminDal.Add(admin);
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Admin> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Admin GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Admin admin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Context/GraduationProjectContext.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class GraduationProjectContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GraduationProject;Trusted_Connection=true");
+        }
+
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Dormitory> Dormitories { get; set; }
+        public DbSet<DormitoryDetail> DormitoryDetails { get; set; }
+        public DbSet<DormitoryOwner> DormitoryOwners { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<University> Universities { get; set; }
+    }
+}
